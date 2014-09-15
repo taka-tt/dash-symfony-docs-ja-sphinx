79991e8361c7e2a0fd8580bb612eda43ff2a055b
Taka <taka717@gmail.com>
2014-09-15 23:46:29 +0900

    Adjust inter-docs link

--- build-docset.sh
+++ build-docset.sh
@@ -19,5 +19,23 @@ git clone git@github.com:iteman/osc2011-nagoya-symfony2-tutorial.git
 # ja ver.
 cd ../tempja
 cp -R ../symfony-docs-ja/* source/
+
+# change doc links
+sed -e "s/:hidden:/:maxdepth: 1/" -i "" source/create-your-framework/index.rst
+
+find ./source/create-your-framework -name "part*.rst" | xargs sed -e "s|http://docs.symfony.gr.jp/symfony2/create-your-framework/|./|g" -i ""
+find ./source/create-your-framework -name "part*.rst" | xargs sed -e "s|http://docs.symfony.gr.jp/symfony2/|../|g" -i ""
+#find ./source/create-your-framework -name "part*.rst" | xargs sed -e "s|http://symfony.com/doc/current/|../|g" -i ""
+#find ./source/create-your-framework -name "part*.rst" | xargs sed -e "s|http://symfony.com/doc|../index.html|g" -i ""
+find ./source/create-your-framework -name "part*.rst" | xargs sed -e "s|http://fr.php.net/|http://php.net/|g" -i ""
+
+find ./source/osc2011-nagoya-symfony2-tutorial -maxdepth 1 -name "*.rst" | xargs sed -e "s|http://docs.symfony.gr.jp/symfony2/|../|g" -i ""
+#find ./source/osc2011-nagoya-symfony2-tutorial -maxdepth 1 -name "part*.rst" | xargs sed -e "s|http://symfony.com/doc/2.0/|../|g" -i ""
+
+find ./source/sf2-blog-tutorial -maxdepth 1 -name "*.rst" | xargs sed -e "s|http://docs.symfony.gr.jp/symfony2/|../|g" -i ""
+#find ./source/sf2-blog-tutorial -maxdepth 1 -name "*.rst" | xargs sed -e "s|http://symfony.com/doc/current/|../|g" -i ""
+find ./source/sf2-blog-tutorial/customize -maxdepth 1 -name "*.rst" | xargs sed -e "s|http://docs.symfony.gr.jp/symfony2/|../../|g" -i ""
+#find ./source/sf2-blog-tutorial/customize -maxdepth 1 -name "*.rst" | xargs sed -e "s|http://symfony.com/doc/current/|../../|g" -i ""
+
 make doc
 
