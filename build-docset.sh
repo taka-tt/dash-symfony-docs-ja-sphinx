f6b57ea481209972fc19f9998ea68d5cffea0a39
Taka <taka717@gmail.com>
2014-09-15 12:18:37 +0900

    Add tutorial links

--- build-docset.sh
+++ build-docset.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-rm -rf symfony-docs-ja
-git clone git://github.com/symfony-japan/symfony-docs-ja.git
+rm -rf sphinx-php symfony-docs-ja
+git clone git@github.com:fabpot/sphinx-php.git
+git clone git@github.com:symfony-japan/symfony-docs-ja.git
 
-cd symfony-docs-ja
+cd sphinx-php
+python setup.py install
+
+cd ../symfony-docs-ja
 git clone git@github.com:symfony-japan/Create-Your-Framework-ja.git create-your-framework
 git clone git@github.com:symfony-japan/sf2-blog-tutorial-doc-ja.git sf2-blog-tutorial
 git clone git@github.com:iteman/osc2011-nagoya-symfony2-tutorial.git
 
+# useless en ver.
 #cd ../temp
 #make html
 
+# ja ver.
 cd ../tempja
 cp -R ../symfony-docs-ja/* source/
 make doc
